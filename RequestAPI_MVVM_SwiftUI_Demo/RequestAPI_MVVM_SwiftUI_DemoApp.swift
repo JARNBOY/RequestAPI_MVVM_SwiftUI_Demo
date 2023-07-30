98704e273db81474600ff5b95e9f078855a8a668
Papon <papon.s@lawyerplusgroup.com>
2023-07-30 17:03:55 +0700

    Initial Commit

---
+++ RequestAPI_MVVM_SwiftUI_Demo/RequestAPI_MVVM_SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequestAPI_MVVM_SwiftUI_DemoApp.swift
+//  RequestAPI_MVVM_SwiftUI_Demo
+//
+//  Created by Papon Supamongkonchai on 30/7/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct RequestAPI_MVVM_SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
